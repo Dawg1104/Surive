d70d20025d77df059171ebac3f3317c315ff0cda
Dylan DAWG <dapickledill76@gmail.com>
2025-08-01 23:37:52 -0700

    Removed Generate.bat

--- Generate.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-premake5 vs2022
-
-REM Replace this path with your actual MSBuild.exe location
-"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" Survive.sln /p:Configuration=Debug /m
-
-pause
